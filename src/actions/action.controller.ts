496246f3fc927413a7fccb14e690b97a17351e55
Karolis Mazukna <karolis@ketapp.lt>
2020-04-26 00:20:11 +0100

    set status code 400

--- src/actions/action.controller.ts
+++ src/actions/action.controller.ts
@@ -1,10 +1,16 @@
-import { Post, JsonController, Body, Req } from "routing-controllers";
+import { Post, JsonController, Body, Req, Res } from "routing-controllers";
+import { Response, Request } from "express";
 
 @JsonController()
 export class ActionController {
   @Post("/actions")
-  public actions(@Body() actionBody: any, @Req() req: any) {
+  public actions(
+    @Body() actionBody: any,
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
     req.log.info(actionBody);
+    res.status(401);
     return {
       message: "Actions code not implemented",
       code: "500",
