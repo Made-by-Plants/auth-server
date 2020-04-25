d368e660af256e81a66966369448994d594fa760
Karolis Mazukna <karolis@ketapp.lt>
2020-04-26 00:14:44 +0100

    update log

--- src/actions/action.controller.ts
+++ src/actions/action.controller.ts
@@ -1,13 +1,13 @@
-import { Post, JsonController, Body } from "routing-controllers";
+import { Post, JsonController, Body, Req } from "routing-controllers";
 
 @JsonController()
 export class ActionController {
   @Post("/actions")
-  public actions(@Body() actionBody: any) {
+  public actions(@Body() actionBody: any, @Req() req: any) {
+    req.log.info(actionBody);
     return {
       message: "Actions code not implemented",
       code: "500",
-      actionBody,
     };
   }
 }
